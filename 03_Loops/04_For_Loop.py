6c0a4aaa33d245bc92f8923ce8734f4568c271c8
Praveen MP <mppraveen.dragon007@gmail.com>
2020-01-19 11:51:36 +0530

    For loop, While loop

---
+++ 03_Loops/04_For_Loop.py
@@ -0,0 +1,93 @@
+# 1.
+# for value in range(5):
+#     print(value)
+# O/p:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+# 2.
+# for value in range(-2, 3):
+#     print(value)
+# O/p:
+# -2
+# -1
+# 0
+# 1
+# 2
+
+# 3.
+# fruits = ['apple', 'oraange', 'mango']
+# for value in fruits:
+#     print(value)
+# O/p:
+# apple
+# oraange
+# mango
+
+# 4.
+# for value in range(-2, 3, 2):
+#     print(value)
+# O/p:
+# -2
+# 0
+# 2
+
+# 5.
+# name = 'Joey'
+# for value in name:
+#     print(value)
+# O/p:
+# J
+# o
+# e
+# y
+
+# 6. Print multiplication table
+# num = input("Enter Number: ")
+# for value in range(1, 11):
+#     multiply = value * int(num)
+#     line = str(num) + " * " + str(value) + " = " + str(multiply)
+#     print(line)
+# O/p:
+# Enter Number: 5
+# 5 * 1 = 5
+# 5 * 2 = 10
+# 5 * 3 = 15
+# 5 * 4 = 20
+# 5 * 5 = 25
+# 5 * 6 = 30
+# 5 * 7 = 35
+# 5 * 8 = 40
+# 5 * 9 = 45
+# 5 * 10 = 50
+
+num = input("Enter Number: ")
+for value in range(1, 21):
+    multiply = value * int(num)
+    line = str(num) + " * " + str(value) + " = " + str(multiply)
+    print(line)
+# O/p:
+# Enter Number: 2
+# 2 * 1 = 2
+# 2 * 2 = 4
+# 2 * 3 = 6
+# 2 * 4 = 8
+# 2 * 5 = 10
+# 2 * 6 = 12
+# 2 * 7 = 14
+# 2 * 8 = 16
+# 2 * 9 = 18
+# 2 * 10 = 20
+# 2 * 11 = 22
+# 2 * 12 = 24
+# 2 * 13 = 26
+# 2 * 14 = 28
+# 2 * 15 = 30
+# 2 * 16 = 32
+# 2 * 17 = 34
+# 2 * 18 = 36
+# 2 * 19 = 38
+# 2 * 20 = 40
---
+++ 03_Loops/05_While_Loop.py
@@ -0,0 +1,84 @@
+# 1.
+# i = 0
+# while(i < 5):
+#     print(i)
+#     i += 1
+# O/p:
+# 0
+# 1
+# 2
+# 3
+# 4
+
+# 2.
+# i = 0
+# while(i < 10):
+#     print(i)
+#     i += 1
+# O/p:
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+# 3.
+# i = 1
+# name = 'Joey'
+# while(i < 10 and name == 'Joey'):
+#     print(i)
+#     i += 1
+# O/p:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+# i = 1
+# name = 'Joeyy'
+# while(i < 10 and name == 'Joey'):
+#     print(i)
+#     i += 1
+# O/p:
+
+# i = 1
+# name = 'Joeyy'
+# while(i < 10 or name == 'Joey'):
+#     print(i)
+#     i += 1
+# O/p:
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+
+# 4.
+while(1):
+    print("Infinite")
+# O/p:
+# Infinite
+# Infinite
+# Infinite
+# Infinite
+# Infinite
+# Infinite
+# Infinite
+# InfiniteTraceback (most recent call last):
+#   File "05_While_Loop.py", line 72, in <module>
+#     print("Infinite")
+# KeyboardInterrupt
