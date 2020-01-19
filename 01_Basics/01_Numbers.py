f77e52402bf7385c25a1409cb21654be6dd26a52
Praveen MP <mppraveen.dragon007@gmail.com>
2020-01-19 05:56:47 +0530

    Numbers, Strings

---
+++ 01_Basics/01_Numbers.py
@@ -0,0 +1,77 @@
+# 1.
+# twenty = 20
+# fifty = 50
+# sum1 = twenty + fifty
+# print(twenty)
+# print(fifty)
+# print(sum1)
+# O/p:
+# 20
+# 50
+# 70
+
+# 2.
+# number1 = -80
+# number2 = 100
+# subtract = number2 + number1
+# print(number1)
+# print(number2)
+# print(subtract)
+# O/p:
+# -80
+# 100
+# 20
+
+# 3.
+# number1 = 2
+# number2 = 50
+# div = number2 / number1
+# print(number1)
+# print(number2)
+# print(div)
+# O/p:
+# 2
+# 50
+# 25.0
+
+# 4.
+# number1 = 2
+# number2 = 50
+# multi = number2 * number1
+# print(number1)
+# print(number2)
+# print(multi)
+# O/p:
+# 2
+# 50
+# 100
+
+# 5.
+# number1 = 2
+# number2 = 50
+# multi = 50 * number1
+# print(number1)
+# print(multi)
+# O/p:
+# 2
+# 100
+
+# 6.
+# number1 = 2
+# number2 = 50
+# result = 50 * number1 + number1
+# print(number1)
+# print(result)
+# O/p:
+# 2
+# 102
+
+# 7.
+# number1 = 2
+# number2 = 50
+# result = 50 * (number1 + number1)
+# print(number1)
+# print(result)
+# O/p:
+# 2
+# 200
---
+++ 01_Basics/02_Strings.py
@@ -0,0 +1,67 @@
+# 1.
+# name = "My name is Joey"
+# date = "17th Jan, 2020"
+# print(name)
+# print(date)
+# O/p:
+# My name is Joey
+# 17th Jan, 2020
+
+# 2.
+# name = "My name is Joey"
+# date = "17th Jan, 2020"
+# longString = "This is a long string and can be of any length"
+# shortString = "a"
+# print(name)
+# print(date)
+# print(longString)
+# print(shortString)
+# O/p:
+# My name is Joey
+# 17th Jan, 2020
+# This is a long string and can be of any length
+# a
+
+# 3.
+# name = "My name is Joey"
+# date = "17th Jan, 2020"
+# long_string = "This is a long string and can be of any length"
+# short_string = "a"
+#
+# new_string = name + date
+# print(new_string)
+# new_string = name + ' ' + date
+# print(new_string)
+# O/P:
+# My name is Joey17th Jan, 2020
+# My name is Joey 17th Jan, 2020
+
+# 4.
+# name = "My name is Joey"
+# date = "17th Jan, 2020"
+# long_string = "This is a long string and can be of any length"
+# short_string = "a"
+#
+# multiply = short_string * 5
+# print(multiply)
+# multiply2 = date * 2
+# print(multiply2)
+# O/P:
+# aaaaa
+# 17th Jan, 202017th Jan, 2020
+
+# 5.
+name = "My name is Joey"
+date = "17th Jan, 2020"
+long_string = "This is a long string and can be of any length"
+short_string = "a"
+
+print(date[5])
+print(date[5:8])
+print(date[0:5])
+print(date[-4:])
+print(date[:-4])
+# O/P:
+# J
+# Jan
+# 17th
