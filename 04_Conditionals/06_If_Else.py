2de507f39990b5703f199fbd1a97785b44e6484c
Praveen MP <mppraveen.dragon007@gmail.com>
2020-01-19 11:54:27 +0530

    If Else, Elif

---
+++ 04_Conditionals/06_If_Else.py
@@ -0,0 +1,35 @@
+# 1.
+# x = 20
+# y = 30
+# if x > y:
+#     print("X is Greater than Y")
+# else:
+#     print("X is Smaller than Y")
+# O/p:
+# X is Smaller than Y
+
+# x = 30
+# y = 20
+# if x > y:
+#     print("X is Greater than Y")
+# else:
+#     print("X is Smaller than Y")
+# O/p:
+# X is Greater than Y
+
+# 2.
+# name = 'Joey'
+# if name == 'Joey':
+#     print("Welcome Joey")
+# else:
+#     print("Unknown")
+# O/p:
+# Welcome Joey
+
+name = 'Joey'
+if name == 'Chandler':
+    print("Welcome Joey")
+else:
+    print("Unknown")
+# O/p:
+# Unknown
---
+++ 04_Conditionals/07_Elif.py
@@ -0,0 +1,22 @@
+# 1.
+# x = 30
+# y = 20
+# if x > y:
+#     print("X is Greater than Y")
+# elif x < y:
+#     print("X is Smaller than Y")
+# elif: x == y:
+#     print("X is Equal to Y")
+# O/p:
+# X is Greater than Y
+
+# x = 30
+# y = 30
+# if x > y:
+#     print("X is Greater than Y")
+# elif x < y:
+#     print("X is Smaller than Y")
+# else:
+#     print("X is Equal to Y")
+# O/p:
+# X is Equal to Y
